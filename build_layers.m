dda834d16ab0958a6ae13b36f388e21f8e2968f6
TadejMurovic <46130688+TadejMurovic@users.noreply.github.com>
2019-09-10 18:35:34 +0200

    Add files via upload

---
+++ build_layers.m
@@ -0,0 +1,292 @@
+clear all; close all;
+LT    = 192;
+SEL_T = 1;
+
+for la = 0:1
+    
+    % Config
+    if la == 0
+        N = LT; % Neurons per hidden layer
+        M = 593;
+        sel = SEL_T; % sel = 1 Ripple Arch; sel = 0 Straight-Forward MPBNN code generation
+    end
+    if la == 1
+        N = 1; % Neurons per hidden layer
+        M = LT;
+        sel = 0; % sel = 1 Ripple Arch; sel = 0 Straight-Forward MPBNN code generation
+    end
+    pathw = strcat('C:\Users\fg7c6w\Desktop\PHD\bin_set\cybersecurity\dump_w_gen',num2str(LT),'_',num2str(la),'.txt');
+    patht = strcat('C:\Users\fg7c6w\Desktop\PHD\bin_set\cybersecurity\dump_t_gen',num2str(LT),'_',num2str(la),'.txt');
+
+
+
+    %--------------------------------------------------------------------------
+    % Read Data
+    fileID = fopen(pathw,'r');
+    formatSpec = '%d';
+    cols = M;
+    rows = N;
+    w = fscanf(fileID,formatSpec,[cols rows])';
+    fclose(fileID);
+    fileID = fopen(patht,'r');
+    formatSpec = '%d';
+    th = fscanf(fileID,formatSpec,[rows 1])';
+    fclose(fileID);   
+
+    if sel == 1
+        % Optimize Order
+        ordered = hamming_genetic(w,5000,1000);
+        word = w(ordered,:);
+    end
+ 
+    if sel == 1
+        n = N;
+        m = M;
+        wts = word';
+        a0 = [];
+        a1 = [];
+        d0 = [];
+        d1 = [];
+        for ni = n 
+            counter = 0;
+            dep = 0;
+            for i = 0:log2(ni)-1
+               dep = dep + 1;
+               for j = 0:ni-1 
+                  if j >= 2^i
+                    counter = counter + 1;    
+                  else
+                    counter = counter + 0;   
+                  end
+               end
+            end
+            a0 = [a0 counter];
+            d0 = [d0 dep];
+
+            counter = 0;
+            dep = 0;
+            for d = 0:log2(ni)-1
+                dep = dep + 1;
+                for i = 0:(ni/2^d - 2) 
+                    counter = counter + 1;       
+                end
+            end  
+            for d = (log2(ni)-1):-1:0
+                dep = dep + 1;
+                for i = 0:(ni/2^d - 1)  
+                    if i > 0
+                       if (mod(i,2) ~= 0)
+                           counter = counter + 0;
+                       else
+                           counter = counter + 1;
+                       end
+                    else
+                       counter = counter + 0; 
+                    end
+                end
+            end
+            a1 = [a1 counter];
+            d1 = [d1 dep];  
+        end
+        levels = dep;
+
+        cat_path = strcat('model_',num2str(la),'.txt'); 
+        fid = fopen(cat_path,'w');
+
+        % Generate
+        fprintf(fid,'`timescale 1ns / 1ps');
+        fprintf(fid,'\n\n');
+        fprintf(fid,strcat('module layer_',num2str(la),'(in, out'));
+        fprintf(fid,');\n\n');
+
+        fprintf(fid,'  input [%d:0] in;\n',m-1);
+        fprintf(fid,'  output reg [%d:0] out;\n',n-1);
+        fprintf(fid,'\n');
+            bits = ceil(log2(m)+1); 
+            bts = ceil(log2(m*n)+1);
+            bts = bts + 1;
+            bits = bts;
+            %levels = ceil(log2(n+1));
+        fprintf(fid,'  wire [%d:0] signal [%d:0];\n',m-1,n-1);
+        fprintf(fid,'  wire [%d:0] difsum [%d:0];\n',bits-1,n-1);
+        fprintf(fid,'  wire [%d:0] cumsum [%d:0][%d:0];\n',bts-1,levels-1,n-1);
+        fprintf(fid,'  wire [%d:0] setsum;\n',bits-1);
+        fprintf(fid,'  wire signed [%d:0] eval [%d:0];\n',bts,n-1);
+        fprintf(fid,'\n\n');
+        fprintf(fid,'  assign signal[0] = in;\n');
+        fprintf(fid,'  assign setsum    = ');
+        for j = 1:m
+            fprintf(fid,'signal[0][%d] ',j-1);
+            if j == m
+                fprintf(fid,';\n\n'); 
+            else
+                fprintf(fid,'+ ');
+            end
+        end
+        wts = [ones(m,1) wts];
+
+        for i = 1:n
+            mask = xor(wts(:,i+1),wts(:,i));
+            mask = mask';
+            mask = fliplr(mask);
+            s(i) = sum(mask); 
+            if i < n
+                fprintf(fid,'  assign signal[%d] = signal[%d] ^ %d''b',i,i-1,m); 
+                for j = 1:m
+                    fprintf(fid,'%d',mask(j));    
+                end
+            end
+            mask = fliplr(mask);
+            fprintf(fid,';\n');
+            fprintf(fid,'  assign difsum[%d] = ',i-1); 
+            for j = 1:m
+                chk = 0;
+                if mask(j) == 1
+                    fprintf(fid,'signal[%d][%d] ',i-1,j-1);
+                    chk = 1;
+                end
+                if j == m || sum(mask(j+1:end)) == 0 
+                    if j == 1
+                      fprintf(fid,'1''b0');     
+                    end
+                    fprintf(fid,';\n'); 
+                    break;
+                else
+                    if chk == 1
+                        fprintf(fid,'+ ');
+                    end
+                end
+            end
+        end
+        fprintf(fid,'\n\n');
+
+        for i = 0:n-1
+            fprintf(fid,'  assign cumsum[%d][%d] = difsum[%d];\n',0,i,i);  
+        end
+        fprintf(fid,'\n\n');
+
+        counter = 0;
+        dep = 0;
+        ni = n;
+        for d = 0:log2(ni)-1
+            dep = dep + 1;
+            seti = 2^(d+1)-1:2^(d+1):ni;
+            for i = 0:ni-1
+                if sum(i==seti)==1
+                    fprintf(fid,'  assign cumsum[%d][%d] = cumsum[%d][%d] + cumsum[%d][%d];\n',dep,i,dep-1,i,dep-1,i-2^(d));  
+                    counter = counter + 1; 
+                else
+                    fprintf(fid,'  assign cumsum[%d][%d] = cumsum[%d][%d];\n',dep,i,dep-1,i);  
+                end
+            end
+        end  
+        for d = ceil(log2(ni))-2:-1:0
+            dep = dep + 1;
+            seti = 2^(d+1):2^(d+1):ni;
+            seti(seti > ni-1) = [];
+            seti = seti + 2^d-1;
+            for i = 0:ni-1
+                if sum(i==seti)==1
+                   fprintf(fid,'  assign cumsum[%d][%d] = cumsum[%d][%d] + cumsum[%d][%d];\n',dep,i,dep-1,i,dep-1,i-2^(d));  
+                   counter = counter + 1; 
+                else
+                   fprintf(fid,'  assign cumsum[%d][%d] = cumsum[%d][%d];\n',dep,i,dep-1,i);  
+                end
+            end
+        end
+        fprintf(fid,'\n\n');
+
+        for i = 0:n-1
+            fprintf(fid,'  assign eval[%d] = $signed(cumsum[%d][%d] << 1''b1 );\n',i,levels-1,i);  
+        end
+        fprintf(fid,'\n\n');
+
+        os = s;
+        oth = th;
+        s = cumsum(s);
+
+        fprintf(fid,'  always @* begin\n');
+        for j = 1:n
+            thresh = th(j)-s(j);    
+            bit    = bts;
+            if thresh >= 0  
+                fprintf(fid,'    out[%d]= (eval[%d]<=($signed(setsum)-$signed(%d''sd%d)));\n',j-1,j-1,bit+1,abs(thresh));
+            else
+                fprintf(fid,'    out[%d]= (eval[%d]<=($signed(setsum)-$signed(-%d''sd%d)));\n',j-1,j-1,bit+1,abs(thresh));
+            end
+        end
+        fprintf(fid,'  end\n');
+        fprintf(fid,'\n\n'); 
+
+        fprintf(fid,'\n\n');  
+        fprintf(fid,'endmodule');
+        fclose(fid);
+    else  
+      layer_s = LT;
+      for i = 1:1
+        % Make standard model verilog code
+        Nn = M;
+        M = N;
+        N = Nn;
+        
+        lay = i;
+        bits = ceil(log2(N+1)); 
+        cat_path = strcat('model_',num2str(la),'.txt'); 
+        fid = fopen(cat_path,'w');
+
+        % Generate
+        fprintf(fid,'`timescale 1ns / 1ps');
+        fprintf(fid,'\n\n');
+        fprintf(fid,strcat('module layer_',num2str(la),'(in, out'));
+        fprintf(fid,');\n\n');
+
+        fprintf(fid,'  input [%d:0] in;\n',N-1);
+        fprintf(fid,'  output reg [%d:0] out;\n',M-1);
+        for ii = 0:M-1
+           %fprintf(fid,'  output reg out%d;\n',i);
+           fprintf(fid,'  reg [%d:0] t%d;\n',bits-1,ii);
+           fprintf(fid,strcat('  reg [%d:0] w%d = %d''b'),N-1,ii,N);
+           for j = 1:N
+               fprintf(fid,'%d',w(ii+1,j)); 
+           end
+           fprintf(fid,';\n');
+           fprintf(fid,'  reg [%d:0] th%d = %d''d%d;\n',bits-1,ii,bits,abs(th(ii+1)));
+           fprintf(fid,'  reg [%d:0] weighted%d;\n',N-1,ii);
+        end
+
+        fprintf(fid,'\n\n');
+        fprintf(fid,'  integer idx;\n\n');
+        fprintf(fid, '  always @* begin\n');
+        fprintf(fid, '    for( idx = 0; idx<%d; idx = idx + 1) begin\n',N);
+        for ii = 0:M-1
+           fprintf(fid,'      weighted%d[idx] = ((w%d[idx])~^(in[idx]));\n',ii,ii); 
+        end
+        fprintf(fid,'    end\n');
+        fprintf(fid,'  end\n');
+
+        fprintf(fid,'\n\n');
+        fprintf(fid, '  always @* begin\n');
+        for ii = 0:M-1
+           fprintf(fid,'    t%d = ',ii); 
+           for j = 0:N-2
+              fprintf(fid, 'weighted%d[%d] + ',ii,j); 
+           end
+           j = j + 1;
+           fprintf(fid, 'weighted%d[%d];\n',ii,j);
+        end
+        fprintf(fid,'  end \n');
+
+        fprintf(fid,'\n\n');
+        fprintf(fid, '  always @* begin\n');
+        for ii = 0:M-1
+           fprintf(fid,'    out[%d] = t%d >= th%d;\n',ii,ii,ii); 
+        end    
+        fprintf(fid,'  end \n');
+
+        fprintf(fid,'endmodule');
+        fclose(fid);  
+      end
+    end
+end
+fclose('all')
+return
---
+++ hamming_genetic.m
@@ -0,0 +1,101 @@
+function ordered = hamming_genetic(set, iter_nb, pop_nb)
+    
+    % Parameters
+    n0       = size(set,1);
+    a        = meshgrid(1:n0);
+    dist     = reshape((sum(xor(set(a,:),set(a',:)),2)),n0,n0);
+    [N,dims] = size(set);
+    [nr,nc]  = size(dist);
+    nf = N;
+    
+    
+    pop = zeros(pop_nb,nf);
+    pop(1,:) = (1:nf);
+    for k = 2:pop_nb
+        pop(k,:) = randperm(nf);
+    end
+    
+           
+    global_min   = Inf;
+    total_dist   = zeros(1,pop_nb);
+    dist_history = zeros(1,iter_nb);
+    tmp_pop = zeros(4,nf);
+    new_pop = zeros(pop_nb,nf);
+
+    
+    figure(1000);
+    for iter = 1:iter_nb
+        % Evaluate Each Population Member (Calculate Total Distance)
+        for p = 1:pop_nb
+            d = dist(pop(p,nf),pop(p,1)); % Closed Path
+            for k = 2:nf
+                d = d + dist(pop(p,k-1),pop(p,k));
+            end
+            total_dist(p) = d;
+        end
+        
+        % Find the Best Route in the Population
+        [min_dist,index] = min(total_dist);
+        if iter == 1
+            best_first = sum(dist(1,:));
+        end
+        dist_history(iter) = min_dist;
+        if min_dist < global_min 
+            global_min = min_dist;
+            optRoute = pop(index,:);
+            subplot(3,1,1);
+            image(set'*100); 
+            title(['Input Configuration']);
+            subplot(3,1,2);
+            image(set(optRoute,:)'*100); 
+            title(['Best Configuration']);
+            subplot(3,1,3);
+            plot(1:iter,dist_history(1:iter),'r','linewidth',2);
+            axis([1 iter_nb 0 best_first]);
+            title(['Best: ',num2str(min_dist)]);
+            drawnow;
+        end
+                       
+        % Genetic Algorithm Operators
+        random_order = randperm(pop_nb);
+        for p = 4:4:pop_nb
+            rtes = pop(random_order(p-3:p),:);
+            dists = total_dist(random_order(p-3:p));
+            [ignore,idx] = min(dists); %#ok
+            bestOf4Route = rtes(idx,:);
+            routeInsertionPoints = sort(ceil(nf*rand(1,2)));
+            I = routeInsertionPoints(1);
+            J = routeInsertionPoints(2);
+            for k = 1:4 % Mutate the Best to get Three New Routes
+                tmp_pop(k,:) = bestOf4Route;
+                switch k
+                    case 2 % Flip
+                        tmp_pop(k,I:J) = tmp_pop(k,J:-1:I);
+                    case 3 % Swap
+                        tmp_pop(k,[I J]) = tmp_pop(k,[J I]);
+                    case 4 % Slide
+                        tmp_pop(k,I:J) = tmp_pop(k,[I+1:J I]);
+                    otherwise % Do Nothing
+                end
+            end
+            new_pop(p-3:p,:) = tmp_pop;
+        end
+        pop = new_pop;
+               
+    end
+    
+    subplot(3,1,1);
+    image(set'*100); 
+    title(['Input Configuration']);
+    subplot(3,1,2);
+    image(set(optRoute,:)'*100); 
+    title(['Best Configuration @ Iteration:',num2str(iter),'']);
+    subplot(3,1,3);
+    plot(1:iter,dist_history(1:iter),'r','linewidth',2);
+    axis([1 iter_nb 0 best_first]);
+    title(['Best: ',num2str(min_dist)]);
+    drawnow;
+    
+    ordered = optRoute;
+    
+end
